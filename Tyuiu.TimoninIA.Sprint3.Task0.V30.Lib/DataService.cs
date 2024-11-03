8fa0202175112eccff714a27425c82b5ac1db772
Ivan Timonin <Tyuiu.TimoninIA@outlook.com>
2024-11-04 00:16:10 +0500

    Add project files.

---
+++ Tyuiu.TimoninIA.Sprint3.Task0.V30.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.TimoninIA.Sprint3.Task0.V30.Lib
+{
+    public class DataService : ISprint3Task0V30
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double multSeries = 1;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                multSeries = multSeries * (i / Math.Pow(Math.Sin(1), -10));
+            }
+            return Math.Round(multSeries, 3);
+        }
+    }
+}
